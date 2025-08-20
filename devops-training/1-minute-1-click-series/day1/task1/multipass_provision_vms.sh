b797370c6e2f5e267c4f6466b7d11ce30de5f682
GitLab Automation <automation@gitlab.local>
2025-08-21 00:37:45 +0100

    DevOps Training Day 1 Task 1 & 2 added

---
+++ devops-training/1-minute-1-click-series/day1/task1/multipass_provision_vms.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+multipass delete --purge k3s-master k3s-worker1 k3s-worker2 2>/dev/null
+
+multipass launch --name k3s-master --cpus 2 --memory 2G --disk 5G
+multipass launch --name k3s-worker1 --cpus 2 --memory 4G --disk 30G
+multipass launch --name k3s-worker2 --cpus 2 --memory 4G --disk 30G
+
+multipass exec k3s-master -- sudo apt update
+multipass exec k3s-master -- sudo apt upgrade
+multipass exec k3s-master -- sudo apt install -y curl wget
+
+multipass exec k3s-master -- bash -c "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION='v1.33.1+k3s1' INSTALL_K3S_EXEC='--disable=traefik' sh -s - --node-taint CriticalAddonsOnly=true:NoExecute"
+
+multipass exec k3s-worker1 -- sudo apt update
+multipass exec k3s-worker1 -- sudo apt install -y curl wget
+
+multipass exec k3s-worker2 -- sudo apt update
+multipass exec k3s-worker2 -- sudo apt install -y curl wget
+
+multipass ls
+
+multipass info k3s-master
+multipass info k3s-worker1
+multipass info k3s-worker2
