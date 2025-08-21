b6bf6f9e71d48d8061b1fe6b2eb52186381ed6c0
GitLab Automation <automation@gitlab.local>
2025-08-21 02:24:12 +0100

    DevOps Training Day 1 Task 1 & 2 added

--- devops-training/1-minute-1-click-series/day1/k3s-cluster-setup/k3s_update_lb_ip_in_localdns.sh
+++ devops-training/1-minute-1-click-series/day1/k3s-cluster-setup/k3s_update_lb_ip_in_localdns.sh
@@ -6,9 +6,9 @@ HOSTS_FILE="/etc/hosts"
 echo "[+] Removing lines matching '$HOST_ENTRY' from $HOSTS_FILE"
 
 # Backup before change
-cp "$HOSTS_FILE" "$HOSTS_FILE.bak"
+sudo cp "$HOSTS_FILE" "$HOSTS_FILE.bak"
 
 # Delete lines containing the host entry
-sed -i '' "/${HOST_ENTRY//./\\.}/d" "$HOSTS_FILE"
+sudo sed -i '' "/${HOST_ENTRY//./\\.}/d" "$HOSTS_FILE"
 
 echo "[+] Done. Backup saved as $HOSTS_FILE.bak"
