5326453d72bdda60f313883e8f047e1d6fb21a59
GitLab Automation <automation@gitlab.local>
2025-09-05 23:29:12 +0100

    pylint Your code has been rated at 9.35/10

--- devops-training/1-click-video-series/day2/deploy-minio-in-kubernetes/minio-deploy.sh
+++ devops-training/1-click-video-series/day2/deploy-minio-in-kubernetes/minio-deploy.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-helm upgrade --install -f minio-helm-chart/values.yaml minio-test ./minio-helm-chart \
-      --set-string namespaceRef="test" \
+if [ -z "$1" ]; then
+  echo "The env is empty, so setting ENV_REF to test (default)"
+  ENV_REF="test"
+else
+   echo "The env is NOT empty, so setting ENV_REF to $1"
+   ENV_REF=$1
+fi
+
+if [ -z "$ENV_REF" ]; then
+  ENV_REF="test"
+fi
+
+# Generate a random password
+PASSWORD=$(head -c 16 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
+
+# Display the password
+echo "Minio Admin user: minioadmin and password is: $PASSWORD"
+
+helm upgrade --install -f minio-helm-chart/values.yaml minio-$ENV_REF ./minio-helm-chart \
+      --set-string namespaceRef="$ENV_REF" \
       --set-string pvcStorageSize="1Gi" \
       --set secrets.MINIO_ROOT_USER=minioadmin \
-      --set secrets.MINIO_ROOT_PASSWORD=miniopassword \
-      --set secrets.MINIO_BROWSER_REDIRECT_URL=https://test-s3.workstation.co.uk \
+      --set secrets.MINIO_ROOT_PASSWORD=$PASSWORD \
+      --set secrets.MINIO_BROWSER_REDIRECT_URL=https://$ENV_REF-s3.workstation.co.uk \
       --set secrets.MINIO_DEFAULT_BUCKETS=test-images \
-      --set-string "uiIngress.hosts[0].host=test-s3.workstation.co.uk" \
-      --set-string "cliIngress.hosts[0].host=test-s3-cli.workstation.co.uk" \
-      --namespace test --create-namespace
+      --set-string "uiIngress.hosts[0].host=$ENV_REF-s3.workstation.co.uk" \
+      --set-string "cliIngress.hosts[0].host=$ENV_REF-s3-cli.workstation.co.uk" \
+      --namespace $ENV_REF --create-namespace
 
-echo "The minio is deployed into Kubernetes successfully"
+echo "The minio is deployed into Kubernetes successfully in the $ENV_REF namespace."
 
 echo "Github repository - https://github.com/bwalia/devops.git"
 
--- devops-training/1-click-video-series/day2/deploy-minio-in-kubernetes/minio-destroy.sh
+++ devops-training/1-click-video-series/day2/deploy-minio-in-kubernetes/minio-destroy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-helm uninstall minio-test --namespace test
-echo "The minio is uninstalled from Kubernetes successfully"
+if [ -z "$1" ]; then
+  echo "The env is empty, so setting ENV_REF to test (default)"
+  ENV_REF="test"
+else
+   echo "The env is NOT empty, so setting ENV_REF to $1"
+   ENV_REF=$1
+fi
+
+if [ -z "$ENV_REF" ]; then
+  ENV_REF="test"
+fi
+
+helm uninstall minio-$ENV_REF --namespace $ENV_REF
+
+echo "The minio is destroyed from Kubernetes successfully in the $ENV_REF namespace."
+
+echo "Github repository - https://github.com/bwalia/devops.git"
